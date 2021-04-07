9ff4bcdcb9e170efe2f6261b663ded872f853017
Thony <thon0101@stud.kea.dk>
2021-04-07 12:17:11 +0100

    ArrayList Complete.

--- src/com/company/myArrayList.java
+++ src/com/company/myArrayList.java
@@ -8,33 +8,38 @@ public class myArrayList {
          Metoden returnerer en boolean:•Hvis strengen ikke findes i arraylistenindsættes strengen i listen og metoden returnerer
          false•Hvis strengen findes i arraylisten returnerer metodentrue og printer ”The String has been found”*/
 
+        //Creating arrayList
         ArrayList<String> contain = new ArrayList<String>();
         contain.add("coffee");
         contain.add("cake");
         contain.add("Rugbrød");
         contain.add("Flødeboller");
 
+        //Creating a Scanner to get the user to inset a word.
         Scanner sc = new Scanner(System.in);
         System.out.println("Please type a word: ");
         String text = sc.nextLine();
 
+        //Calling the method from line 30.
         doesArraylistContainString(contain,text);
 
         System.out.println(contain);
 
     }
-
+    //Creating a method with to parameters(type and name, type and name)
     public static boolean doesArraylistContainString(ArrayList list, String text) {
+        //Creating a forloop to check out arrayList.
         for (int i = 0; i < list.size(); i++) {
-
+            //Using If statement to get the userinput and se if the input has been found.
             if (text.equals(list.get(i))) {
                 System.out.println("The String has been found");
                 return true;
             }
         }
-
+        /*If the new text is not on the list it will be added to the list and line 41 will be printet out and the new
+        list wil be updatet with the input from user.*/
         list.add(text);
-        System.out.println("The String has not been found in the original list but wil be added.");
+        System.out.println("The String has not been found in the original list but will be added.");
         return false;
     }
 }
