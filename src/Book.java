4303b4d0a5acc08b224fbe51d17fcaa99e3cd8ce
Thony <thon0101@stud.kea.dk>
2021-04-07 16:04:39 +0100

    Book allmostComplete.

---
+++ src/Book.java
@@ -0,0 +1,30 @@
+public class Book {
+
+    //Attributes
+    public int ISBN;
+    public String title;
+    public int releaseyear;
+    //Constructor
+    public Book(int ISBN, String title, int releaseyear) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.releaseyear = releaseyear;
+
+    }
+    //Getter
+    public int getISBN() {
+        return ISBN;
+    }
+    //Getter
+    public String getTitle() {
+        return title;
+    }
+    //Getter
+    public int getReleaseyear() {
+        return releaseyear;
+    }
+    @Override
+    public String toString() {
+        return "BookDate: " + "ISBN" + ISBN + "ReleaseYear" + releaseyear + "Title" + title;
+    }
+}
---
+++ src/Library.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Library {
+    public static void main(String[] args) {
+        //Creating objects.
+        Book BuildingJavaEditionOne = new Book(123456, "Building Java Edition One", 2019);
+        Book BuildingJavaEditionTwo = new Book(1234567, "Building Java Edition Two", 2020);
+        Book BuildingJavaEditionThree = new Book(12345678, "Building Java Edition Three", 2021);
+        Book BuildingJavaEditionFour = new Book(123456, "Building Java Edition Four", 2022);
+
+        //Creating ArrayList.
+        ArrayList<Book> bookList = new ArrayList<>();
+        bookList.add(BuildingJavaEditionOne);
+        bookList.add(BuildingJavaEditionTwo);
+        bookList.add(BuildingJavaEditionThree);
+        bookList.add(BuildingJavaEditionFour);
+        System.out.println(bogSameISBN(BuildingJavaEditionTwo,bookList));
+
+
+    }
+
+    //Creating Method
+    public static boolean bogSameISBN(Book sameBook, ArrayList<Book> list) {
+        for (Book book : list) {
+
+            if (book.equals(sameBook)) {
+                System.out.println("Library contains \"" + sameBook.getTitle() + "\" with the given ISBN: " + sameBook.getISBN());
+                return true;
+            }
+        }
+            System.out.println("Library do not contain \"" + sameBook.getTitle() + "\" with the given ISBN: " + sameBook.getISBN());
+            return false;
+        }
+    }
+
+
+
